8cd62510fa202cfc111cc3f5ec3d1650f509b909
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-09-11 15:25:34 -0500

    set up models for Agent, Customer, & Order

---
+++ orders/src/main/java/com/lambdaschool/orders/models/Order.java
@@ -0,0 +1,86 @@
+package com.lambdaschool.orders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long ordnum;
+
+    private double ordamount;
+    private double advanceamount;
+    private String orddescription;
+
+    @ManyToOne
+    @JoinColumn(name = "custcode", nullable = false)
+    @JsonIgnoreProperties("orders")
+    private Customer customer;
+
+    public Order()
+    {
+    }
+
+    public Order(double ordamount, double advanceamount, String orddescription, Customer customer)
+    {
+        this.ordamount = ordamount;
+        this.advanceamount = advanceamount;
+        this.orddescription = orddescription;
+        this.customer = customer;
+    }
+
+    public long getOrdnum()
+    {
+        return ordnum;
+    }
+
+    public void setOrdnum(long ordnum)
+    {
+        this.ordnum = ordnum;
+    }
+
+    public double getOrdamount()
+    {
+        return ordamount;
+    }
+
+    public void setOrdamount(double ordamount)
+    {
+        this.ordamount = ordamount;
+    }
+
+    public double getAdvanceamount()
+    {
+        return advanceamount;
+    }
+
+    public void setAdvanceamount(double advanceamount)
+    {
+        this.advanceamount = advanceamount;
+    }
+
+    public String getOrddescription()
+    {
+        return orddescription;
+    }
+
+    public void setOrddescription(String orddescription)
+    {
+        this.orddescription = orddescription;
+    }
+
+    public Customer getCustomer()
+    {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer)
+    {
+        this.customer = customer;
+    }
+}
